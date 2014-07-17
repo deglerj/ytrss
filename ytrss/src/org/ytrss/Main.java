e5e6f07a7bb236e87eeb1760b0586a9200810051
deglerj <johannes@degler-online.de>
2014-07-17 17:57:30 +0200

    Create all required directories on first startup

--- ytrss/src/org/ytrss/Main.java
+++ ytrss/src/org/ytrss/Main.java
@@ -1,5 +1,8 @@
 package org.ytrss;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +16,8 @@ import org.ytrss.youtube.StreamMapEntryScorer;
 //Based on: https://github.com/jasonish/jetty-springmvc-jsp-template
 public class Main {
 
-	public static void main(final String[] args) throws Exception {
+	public static void main(final String[] args) throws IOException {
+		createDirectories();
 
 		try {
 			@SuppressWarnings("resource")
@@ -45,6 +49,25 @@ public class Main {
 		catch (final Exception e) {
 			log.error("Error starting application", e);
 			System.exit(1);
+		}
+	}
+
+	private static void createDirectories() throws IOException {
+		final String baseDirectory = System.getProperty("user.home") + "/.ytrss";
+
+		final File data = new File(baseDirectory + "/data");
+		if (!data.exists()) {
+			data.mkdirs();
+		}
+
+		final File videos = new File(baseDirectory + "/videos");
+		if (!videos.exists()) {
+			videos.mkdirs();
+		}
+
+		final File mp3s = new File(baseDirectory + "/mp3s");
+		if (!mp3s.exists()) {
+			mp3s.mkdirs();
 		}
 	}
 
