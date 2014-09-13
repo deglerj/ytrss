e781e79624293bf556d3b30fa4969276cbbf2e8d
deglerj <johannes@degler-online.de>
2014-09-13 10:35:46 +0200

    Added support for Spring 4.1

--- ytrss/src/org/ytrss/config/AsyncConfiguration.java
+++ ytrss/src/org/ytrss/config/AsyncConfiguration.java
@@ -4,6 +4,9 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +15,7 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.ytrss.Ripper;
 import org.ytrss.db.SettingsService;
 
 @Configuration
@@ -19,12 +23,19 @@ import org.ytrss.db.SettingsService;
 @EnableAsync
 public class AsyncConfiguration implements AsyncConfigurer {
 
+	private static Logger	log	= LoggerFactory.getLogger(Ripper.class);
+
 	@Autowired
 	private SettingsService	settingsService;
 
 	@Override
 	public Executor getAsyncExecutor() {
 		return new SimpleAsyncTaskExecutor();
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return (ex, method, params) -> log.error(String.format("Unexpected error occurred invoking async " + "method '%s'.", method), ex);
 	}
 
 	@Bean
