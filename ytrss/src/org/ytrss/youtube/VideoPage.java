8dc39200737210cf6f2d5fdad16fed3242dfe225
deglerj <johannes@degler-online.de>
2014-09-28 08:39:59 +0200

    Adapted a slight change in YouTube's HTML code

--- ytrss/src/org/ytrss/youtube/VideoPage.java
+++ ytrss/src/org/ytrss/youtube/VideoPage.java
@@ -38,7 +38,7 @@ public class VideoPage {
 
 	private static final Pattern	KEY_VALUE_PATTERN	= Pattern.compile("^([^=]+)=(.+)$");
 
-	private static final Pattern	UPLOADED_PATTERN	= Pattern.compile("<strong>.+on\\s+([^<]+)");
+	private static final Pattern	UPLOADED_PATTERN	= Pattern.compile("<strong[^>]*>.+on\\s+([^<]+)");
 
 	private static Logger			log					= LoggerFactory.getLogger(ChannelController.class);
 
