d5962e8e1675025950ead9879b1e931cdfefe94d
deglerj <johannes@degler-online.de>
2014-10-24 09:20:01 +0200

    Improved JS readability

--- ytrss/resources/webapp/WEB-INF/js/ytrss.js
+++ ytrss/resources/webapp/WEB-INF/js/ytrss.js
@@ -96,92 +96,136 @@ function updateTableContent(table, videos) {
 		
 		$(tds[0]).text(video.uploaded);
 
+		//Show channel name? Add link to channel to first cell
 		if(showChannels){
 			$(tds[1]).html('<a href="channel/' + video.channelID + '">' + video.channelName + '</a>');
 		}
 		
+		//Add link to YouTube video to next cell
 		$(tds[showChannels ? 2 : 1]).html('<a href="http://youtube.com/watch?v=' + video.youtubeID + '">' + video.name + '</a>');
 		
-		var state = '';
-		switch(video.state) {
-		case 3: case 6:
-			state += '<a class="table-state-label" href="#" onclick="window.alert(\'' + video.errorMessage + '\')">'; break;
-		case 7:
-			state += '<a class="table-state-label" href="download?id=' + video.id + '&token=' + video.securityToken + '">'; break;
-		}
-		state += '<span class="label table-state-label ';
-		switch(video.state) {
-		case 0: case 2: case 5:
-			state += 'label-info'; break;
-		case 3: case 6:
-			state += 'label-danger'; break;
-		case 7:
-			state += 'label-success'; break;
-		default:
-			state += 'label-default'; break;
-		}
-		state += '">';
-		state += '<i class="glyphicon ';
-		switch(video.state) {
-		case 0: 
-			state += 'glyphicon-info-sign'; break;
-		case 1: case 4: 
-			state += 'glyphicon-time'; break;
-		case 2:
-			state += 'glyphicon-download'; break;
-		case 3: case 6:
-			state += 'glyphicon-warning-sign'; break; 
-		case 5:
-			state += 'glyphicon-record'; break;
-		case 7:
-			state += 'glyphicon-ok'; break;
-		case 8:
-			state += 'glyphicon glyphicon-remove'; break;	
-		}
-		state += '"></i>';
-		switch(video.state) {
-		case 0: 
-			state += 'New'; break;
-		case 1: 
-			state += 'DL enqueued'; break;
-		case 2: 
-			state += 'Downloading'; break;
-		case 3: 
-			state += 'DL failed'; break;
-		case 4: 
-			state += 'TRC enqueued'; break;
-		case 5: 
-			state += 'Transcoding'; break;
-		case 6: 
-			state += 'TRC failed'; break;
-		case 7: 
-			state += 'Ready'; break;
-		case 8:
-			state += 'Deleted'; break;
-		}
-		state += '</span>';
-		if(video.state == 3 ||video.state == 6 || video.state == 7) {
-			state += '</a>';
-		}
-		$(tds[showChannels ? 3 : 2]).html(state);
+		//Add video state information to next cell
+		$(tds[showChannels ? 3 : 2]).html(createStateCell(video));
 		
-		var showOptions = false;
-		var options = '<div class="dropdown"><span class="dropdown-toggle glyphicon glyphicon-th-large" style="cursor:pointer" id="videoOptionMenu' + video.id +'" data-toggle="dropdown"></span><ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="videoOptionMenu' + video.id + '">';		
-		if(video.state == 7){
-			options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="stream(' + video.id + ')"><span class="glyphicon glyphicon-play"></span> Stream</a></li>';
+		//Is at least one option available for the video? -> Add options cell
+		if(hasOptionsCell(video)) {
+			$(tds[showChannels ? 4 : 3]).html(createOptionsCell(video));
 		}
-		if(video.state == 4 || video.state == 6 || video.state == 7 || video.state == 8){
-			options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="resetVideo(' + video.id + ')"><span class="glyphicon glyphicon-repeat"></span> Reset</a></li>';
-			showOptions = true;
+		//No options available? -> Add deactivated options cell
+		else {
+			$(tds[showChannels ? 4 : 3]).html('<span class="glyphicon glyphicon-th-large" style="opacity:0.05;filter:alpha(opacity=05)"></span>');
 		}
-		if(video.state != 2 && video.state != 5 && video.state != 8){
-			options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="deleteVideo(' + video.id + ')"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>';
-			showOptions = true;
-		}
-		options += '</ul></div>';
-		
-		$(tds[showChannels ? 4 : 3]).html(showOptions ? options : '<span class="glyphicon glyphicon-th-large" style="opacity:0.05;filter:alpha(opacity=05)"></span>');
 	});
+}
+
+function hasOptionsCell(video) {
+	//Reset option available?
+	if(video.state == 4 || video.state == 6 || video.state == 7 || video.state == 8) {
+		return true;
+	}
+	
+	//Delete option available?
+	if(video.state != 2 && video.state != 5 && video.state != 8) {
+		
+	}
+}
+
+function createOptionsCell(video) {
+	var options = '<div class="dropdown"><span class="dropdown-toggle glyphicon glyphicon-th-large" style="cursor:pointer" id="videoOptionMenu' + video.id +'" data-toggle="dropdown"></span><ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="videoOptionMenu' + video.id + '">';		
+	
+	//Stream option available?
+	if(video.state == 7){
+		options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="stream(' + video.id + ')"><span class="glyphicon glyphicon-play"></span> Stream</a></li>';
+	}
+	
+	//Reset option available?
+	if(video.state == 4 || video.state == 6 || video.state == 7 || video.state == 8) {
+		options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="resetVideo(' + video.id + ')"><span class="glyphicon glyphicon-repeat"></span> Reset</a></li>';
+	}
+	
+	//Delete option available?
+	if(video.state != 2 && video.state != 5 && video.state != 8) {
+		options += '<li role="presentation"><a role="menuitem" tabindex="-1" style="text-decoration: none" href="javascript:void(0)" onclick="deleteVideo(' + video.id + ')"><span class="glyphicon glyphicon-trash"></span> Delete</a></li>';
+	}
+	
+	options += '</ul></div>';
+	return options;
+}
+
+function createStateCell(video) {
+	var state = '';
+	
+	//Add a download link (if the video has been transcoded) or an error popup link (if an error occured) 
+	switch(video.state) {
+	case 3: case 6:
+		state += '<a class="table-state-label" href="#" onclick="window.alert(\'' + video.errorMessage + '\')">'; break;
+	case 7:
+		state += '<a class="table-state-label" href="download?id=' + video.id + '&token=' + video.securityToken + '">'; break;
+	}
+		
+	//Add CSS style matching the video's state
+	state += '<span class="label table-state-label ';
+	switch(video.state) {
+	case 0: case 2: case 5:
+		state += 'label-info'; break;
+	case 3: case 6:
+		state += 'label-danger'; break;
+	case 7:
+		state += 'label-success'; break;
+	default:
+		state += 'label-default'; break;
+	}
+	state += '">';
+
+	//Add icon matching the video's state
+	state += '<i class="glyphicon ';
+	switch(video.state) {
+	case 0: 
+		state += 'glyphicon-info-sign'; break;
+	case 1: case 4: 
+		state += 'glyphicon-time'; break;
+	case 2:
+		state += 'glyphicon-download'; break;
+	case 3: case 6:
+		state += 'glyphicon-warning-sign'; break; 
+	case 5:
+		state += 'glyphicon-record'; break;
+	case 7:
+		state += 'glyphicon-ok'; break;
+	case 8:
+		state += 'glyphicon glyphicon-remove'; break;	
+	}
+	state += '"></i>';
+	
+	//Add text matching the video's state
+	switch(video.state) {
+	case 0: 
+		state += 'New'; break;
+	case 1: 
+		state += 'DL enqueued'; break;
+	case 2: 
+		state += 'Downloading'; break;
+	case 3: 
+		state += 'DL failed'; break;
+	case 4: 
+		state += 'TRC enqueued'; break;
+	case 5: 
+		state += 'Transcoding'; break;
+	case 6: 
+		state += 'TRC failed'; break;
+	case 7: 
+		state += 'Ready'; break;
+	case 8:
+		state += 'Deleted'; break;
+	}
+	state += '</span>';
+	
+	//Added a download or error popup link? -> Close the tag
+	if(video.state == 3 ||video.state == 6 || video.state == 7) {
+		state += '</a>';
+	}
+	
+	return state;
 }
 
 function resetVideo(id) {
